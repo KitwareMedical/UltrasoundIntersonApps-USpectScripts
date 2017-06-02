3a25987f52d79fa957a20fc62104951523971bbd
Matt McCormick <matt.mccormick@kitware.com>
2017-06-02 17:17:05 -0400

    ENH: Add spacing information to LinearArray B-modes

--- Convert_USRF_BMode.py
+++ Convert_USRF_BMode.py
@@ -28,12 +28,27 @@ def Convert_USRF_BMode(inputFilePath):
     bMode_Filter.Update()
     bMode = bMode_Filter.GetOutput()
 
-    outputFilePath = Out_Dir + Input_FileName + '_BMode.mha'
+    writerInput = bMode
+    # Apply missing spacing information
+    if 'LinearProbe' in inputFilePath:
+        changeInformation = itk.ChangeInformationImageFilter.New(bMode)
+        changeInformation.SetUseReferenceImage(True)
+        changeInformation.SetChangeSpacing(True)
+        output_spacing = [1.0, 1.0]
+        size = itk.size(bMode)
+        sos = 1540.
+        fs = 30000.
+        output_spacing[0] = sos / (2 * fs)
+        transducer_width = 30.0
+        output_spacing[1] = transducer_width / (size[1] - 1)
+        changeInformation.SetOutputSpacing(output_spacing)
+        writerInput = changeInformation.GetOutput()
 
+    outputFilePath = Out_Dir + Input_FileName + '_BMode.mha'
     print(outputFilePath)
     writer = itk.ImageFileWriter[ImageType].New()
     writer.SetFileName(outputFilePath)
-    writer.SetInput(bMode)
+    writer.SetInput(writerInput)
     writer.SetUseCompression(True)
     writer.Update()
 
